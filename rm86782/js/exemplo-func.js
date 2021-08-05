f4b7e70aaa8651aea73be7a0f40f108591a64e99
MFTECH-code <51761169+MFTECH-code@users.noreply.github.com>
2021-08-05 10:53:48 -0300

    Arrow function

--- rm86782/js/exemplo-func.js
+++ rm86782/js/exemplo-func.js
@@ -3,5 +3,9 @@ function soma(a, b) {
     return a + b
 }
 
+// Arrow function
+const subtracao = (a, b) => a - b
+
 // Apresentando valor no console
-console.log(soma(10, 10))
+console.log("Resultado da soma: " + soma(10, 10))
+console.log("Resultado da subtração: " + subtracao(9, 5))
