b97a758668714188e003d98c326345f574659d38
MFTECH-code <51761169+MFTECH-code@users.noreply.github.com>
2021-08-05 12:06:07 -0300

    Criando primeira aplicação React

---
+++ rm86782/aula01/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Java é melhor</h1>
+        <p>Olá, react!!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
