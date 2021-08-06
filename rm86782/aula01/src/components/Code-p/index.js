a92d44815a8e31f22b742df3f4e17b832f2b3bbc
MFTECH-code <51761169+MFTECH-code@users.noreply.github.com>
2021-08-06 09:02:40 -0300

    Usando variaveis no JSX

--- rm86782/aula01/src/components/Code-p/index.js
+++ rm86782/aula01/src/components/Code-p/index.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 export default function Code() {
+    const nome = "Matheus";
+    const curso = "Análise e desenvolvimento de sistemas";
+    const turma = "1TDSG";
     return(
-        <p><code>Java</code> é a melhor linguagem...</p>
+        <>
+            <p><code>Java</code> é a melhor linguagem...</p>
+            <p>Nome: {nome}</p>
+            <p>Curso: {curso}</p>
+            <p>Turma: {turma}</p>
+        </>
     );
 }
