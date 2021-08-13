78f96687e37e7f0a66ba89d2a2540b33724c893e
Matheus Sanches <rm87426@fiap.com.br>
2021-08-13 09:45:48 -0300

    Utilização do props, recebendo da classe pai

--- rm87426/exercicio/src/componentes/carro/Carro.js
+++ rm87426/exercicio/src/componentes/carro/Carro.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import logo from '../img/carro.jpg'
 
-export default function Carro()
+export default function Carro(props)
 {
+    const novoNome = 'CARROS'
     return(
         <>
         <img src = {logo} className = "carroImagem" alt = "Lamborghini"/>
+
+        <h2>{props.alteraNome(novoNome)}</h2>
+
         <ul>
-            <li>Lamborghini Miura</li>
-            <li>Lamborghini LM002</li>
-            <li>Lamborghini Diablo</li>
-            <li>Lamborghini Murcielago SV</li>
-            <li>Lamborghini Veneno</li>
+            <li>{props.carrosAttrib[0]}</li>
+            <li>{props.carrosAttrib[1]}</li>
+            <li>{props.carrosAttrib[2]}</li>
+            <li>{props.carrosAttrib[3]}</li>
+            <li>{props.carrosAttrib[4]}</li>
         </ul>
         </>
     );
