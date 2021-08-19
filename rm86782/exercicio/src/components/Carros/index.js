12e9ece370f9a1ba5cf66e69ba91a0f36c4fad40
MFTECH-code <51761169+MFTECH-code@users.noreply.github.com>
2021-08-19 10:50:44 -0300

    Iterando com map

--- rm86782/exercicio/src/components/Carros/index.js
+++ rm86782/exercicio/src/components/Carros/index.js
@@ -3,17 +3,19 @@ import carroProblema from "../../img/carroProblema.jpg";
 import "./style.css";
 
 export default function Carros(props) {
+    const lista = props.carrosAtrib.map((e, i) => {
+        return <li key={i}>{e}</li>
+    })
+
+
+
     // Componente filho
     return(
         <section className="card">
             <h2 className="card-title">{props.nmLista}</h2>
             <img className="card-img" src={carroProblema} alt="Carro mais problemático do Brasil"/>
             <ul className="lista-carros">
-                <li>{props.carrosAtrib[0]}</li>
-                <li>{props.carrosAtrib[1]}</li>
-                <li>{props.carrosAtrib[2]}</li>
-                <li>{props.carrosAtrib[3]}</li>
-                <li>{props.carrosAtrib[4]}</li>
+                {lista}
             </ul>
         </section>
     );
