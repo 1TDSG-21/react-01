43bbd49b874e0a12badfc772190b439586c99e20
Matheus Sanches <rm87426@fiap.com.br>
2021-08-19 22:25:09 -0300

    Inicio do projeto e criação de componente principal

---
+++ rm87426/exercicio2/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './components/cabecalho/Header'
+import Corpo from './components/corpo/Corpo'
+import Footer from './components/rodape/Footer'
+
+export default function App() {
+    return (
+        <>
+        <div>
+            <Header/>
+            <Corpo/>
+            <Footer/>
+        </div>
+        </>
+    )
+}
