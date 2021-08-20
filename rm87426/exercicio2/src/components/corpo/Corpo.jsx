ebd457de27debf7a2e1477947fb2ab0729a9e594
Matheus Sanches <rm87426@fiap.com.br>
2021-08-19 22:25:55 -0300

    Criação do corpo do projeto

---
+++ rm87426/exercicio2/src/components/corpo/Corpo.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Imagem from '../imagem/Imagem'
+
+export default function Corpo() {
+    return (
+        <body>
+            <h3>Isto é um subtitulo</h3>
+            <Imagem/>
+            <p>Vai Corinthians!</p>
+            <p>FIAP</p>
+            <p>Matheus Sanches</p>
+            <p>React.js</p>
+        </body>
+    )
+}
