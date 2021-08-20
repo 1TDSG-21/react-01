8b62473487908732feca3c15c073e6b5f55450d5
Matheus Sanches <rm87426@fiap.com.br>
2021-08-20 09:46:14 -0300

    Aula sobre State e correção dos exs da ultima aula

--- rm87426/exercicio2/src/components/corpo/Corpo.jsx
+++ rm87426/exercicio2/src/components/corpo/Corpo.jsx
@@ -1,16 +1,31 @@
-import React from 'react'
-import Imagem from '../imagem/Imagem'
+import React, { useState } from 'react'
+import capa_adesivos from '../../capa_adesivos.jpg'
+import Lista from '../../../lista/Lista'
+import './Corpo.css'
 
 export default function Corpo() {
+    const [aluno, setAluno] = useState("Matheus")
     return (
         <>
         <section className="corpo">
             <h3>Isto é um subtitulo</h3>
-            <Imagem/>
+            <img src={capa_adesivos} alt="capa com adesivos" />
+
+            <div>
             <p>Vai Corinthians!</p>
             <p>FIAP</p>
             <p>Matheus Sanches</p>
             <p>React.js</p>
+            </div>
+
+            <div>
+                <p>Exemplo de STATE</p>
+                <p>Nome aluno: {aluno}</p>
+                <button onClick={() => setAluno("Sanches")} onDoubleClick = {() => setAluno("Barreiros")}>Alterar</button>
+            </div>
+
+            <Lista/>
+
         </section>
         </>
     )
---
+++ rm87426/exercicio2/src/components/item/Item.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Item(props) {
+    return (
+        <div className="resultado">
+            <p>Resultado da pesquisa: {props.valorResultado}</p>
+
+            <button onClick={() => props.pesquisar()}>INCREMENTAR-NO-FILHO</button>
+        </div>
+    )
+}
