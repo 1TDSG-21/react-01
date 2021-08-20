eca2bdb5f7529268048e2edc7022abb95561cc9f
Matheus Sanches <rm87426@fiap.com.br>
2021-08-19 22:25:42 -0300

    Criação de componente header

---
+++ rm87426/exercicio2/src/components/cabecalho/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <header>
+            <h1>Aprendendo React.JS</h1>
+            <p>Estes codigos servem para aprendizado de React!</p>
+        </header>
+    )
+}
