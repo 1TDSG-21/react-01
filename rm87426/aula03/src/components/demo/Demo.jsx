cf72d9eed9b4fba7ad1c784f7a894df77b605bc8
DiegoSousaRodrigues2 <rm87910@fiap.com.br>
2021-09-03 18:12:43 -0300

    ta indo

--- rm87426/aula03/src/components/demo/Demo.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Demo() {
-    return (
-        <section>
-            <h1>Essa é a Demo</h1>
-        </section>
-    )
-}
--- rm87426/aula03/src/components/resource/Resource.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Resource() {
-    return (
-        <section>
-            <h1>Esse é o Resource</h1>
-        </section>
-    )
-}
