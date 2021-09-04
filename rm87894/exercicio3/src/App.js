fb472790c8e1016d900b3eca5627b47e65388acc
JP <80105602+JP3015@users.noreply.github.com>
2021-09-03 22:07:24 -0300

    Fiz o máximo que pude, tu hiper cansado
    :)

--- rm87894/exercicio3/src/App.js
+++ rm87894/exercicio3/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import Semaforo from './components/Semaforo/semaforo'
+import Carro from './components/carros/carros'
+import Carterinha from './components/Carterinha/carterinha'
+
 
 const App = () =>{
     return(
         <div className= 'container'>
               <h1>RM87894 - TDSG</h1>
-              <Semaforo />       
+              <Semaforo />
+              <Carro />
+              <Carterinha />       
         </div>
 
     )
