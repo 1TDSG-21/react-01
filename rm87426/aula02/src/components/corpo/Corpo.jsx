04a03b1328117c84b70492384181da5395dcc15c
Matheus Sanches <rm87426@fiap.com.br>
2021-09-06 11:27:23 -0300

    Exercicio finalizado

--- rm87426/aula02/src/components/corpo/Corpo.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Corpo() {
-    return (
-        <section className="sec-1">
-            Estilizando o filho com CSS pai
-        </section>
-    )
-}
