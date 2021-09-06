b3ae37b55c71430b107ec62b83f1ec63cff03106
Matheus Sanches <rm87426@fiap.com.br>
2021-09-06 11:11:32 -0300

    Exercicio finalizado, nao consegui fazer o 5

---
+++ rm87426/exercicio3/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Cabecalho from './components/cabecalho/Cabecalho';
+import Carro from './components/carro/Carro';
+import Carteirinha from './components/carteirinha/Carteirinha';
+
+function App() {
+
+  return (
+    <div className="App">
+        <h1>Matheus Sanches - 87426 - 1TDSG</h1>
+        <Cabecalho/>
+        <Carro/>
+        <Carteirinha/>
+    </div>
+  );
+}
+
+export default App;
