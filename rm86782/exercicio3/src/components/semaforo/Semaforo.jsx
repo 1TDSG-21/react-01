84279308801dc29cef7c2d82a8e54f563b4dcd6f
MFTECH-code <51761169+MFTECH-code@users.noreply.github.com>
2021-09-09 11:02:22 -0300

    Finalização do componente semaforo

--- rm86782/exercicio3/src/components/semaforo/Semaforo.jsx
+++ rm86782/exercicio3/src/components/semaforo/Semaforo.jsx
@@ -1,13 +1,28 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "./semaforo.css"
 
 
 export default function Semaforo() {
+    const [semaforoColor, setSemaforoColor] = useState("red")
+    const [sinal, setSinal] = useState("Pare")
+
+    const switchColor = () => {
+        if (semaforoColor === "red") {
+            setSemaforoColor("green")
+            setSinal("Siga")
+        } else if (semaforoColor === "green") {
+            setSemaforoColor("yellow")
+            setSinal("Atenção")
+        } else {
+            setSemaforoColor("red")
+            setSinal("Pare")
+        }
+    }
+
     return (
         <div>
-            <h2 className="red">Pare</h2>
-            <h2 className="yellow">Atenção</h2>
-            <h2 className="green">Siga</h2>
+            <h2 className={semaforoColor}>{sinal}</h2>
+            <button onClick={() => switchColor()}>Trocar</button>
         </div>
     )
 }
