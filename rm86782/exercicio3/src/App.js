e52bdd8c007fb198a43610fb0618de6e30768b9e
MFTECH-code <51761169+MFTECH-code@users.noreply.github.com>
2021-09-09 10:44:32 -0300

    Criação do projeto

---
+++ rm86782/exercicio3/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Matheus Feitosa Silva | RM86782 | TDSG manhã</h1>
+    </div>
+  );
+}
+
+export default App;
