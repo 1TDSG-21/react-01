47c25e0c4b0aa7da7e71d6c1fca4a6692f15d76f
MFTECH-code <51761169+MFTECH-code@users.noreply.github.com>
2021-09-10 09:24:46 -0300

    Sobre minha profissão

--- rm86782/osite/src/components/topo/Topo.jsx
+++ rm86782/osite/src/components/topo/Topo.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 export default function Topo() {
     return (
         <header>
-            <h1>Topo</h1>
+            <h1>Atendente receptivo de telemarketing</h1>
+            <p>
+                Atendo clientes de uma grande instituição financeira sobre problemas com cartão de crédito, pagamentos, habilitação de aplicativo e desbloqueio de cartões.
+            </p>
+            <a href="#servicos">Ver mais</a>
         </header>
     )
 }
